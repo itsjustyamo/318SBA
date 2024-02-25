2d39df5b8d41e594f5f1fd36f4eb3bc1cfcf562f
Tshiamo Tsoku <ttsoku@icloud.com>
2024-02-25 11:47:46 -0800

    Third commit

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const logger = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+
+
+// Middleware
+app.use(logger);
+app.use(express.json()); // Parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
 const users = require("./routes/users");
 const posts = require("./routes/posts");
@@ -8,3 +15,36 @@ const error = require("./utilities/error");
 
 const app = express();
 const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+// // Routes
+// app.use('/api', require('./routes/api'));
+
+// // Error handling middleware
+// app.use(errorHandler);
+
+// Start the server
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+// // Get all resources
+// router.get('/resources', (req, res) => {
+//     // Logic to retrieve all resources
+//     res.json({ message: 'Get all resources' });
+// });
+
+// // Create a new resource
+// router.post('/resources', (req, res) => {
+//     // Logic to create a new resource
+//     res.json({ message: 'Create a new resource' });
+// });
