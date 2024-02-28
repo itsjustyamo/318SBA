4c63301cc05b325787ed6469fa55b3dc863b3f4a
Tshiamo Tsoku <ttsoku@icloud.com>
2024-02-28 13:05:22 -0800

    Working Version

--- server.js
+++ server.js
@@ -7,62 +7,42 @@ const port = 3000;
 // Middleware
 app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
- 
-const lipstick = require("./routes/lipstick");
-const mascara = require("./routes/mascara");
-const srunchie = require("./routes/scrunchie");
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-// Logging Middlewaare
+// Logging Middleware
 app.use((req, res, next) => {
     const time = new Date();
-
-    console.log(
-        `-----
-    ${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`
-      );
-      if (Object.keys(req.body).length > 0) {
+    console.log(`${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`);
+    if (Object.keys(req.body).length > 0) {
         console.log("Containing the data:");
         console.log(`${JSON.stringify(req.body)}`);
-      }
-      next();
-    });
+    }
+    next();
+});
 
-// 404 Miidleware
+// View engine setup
+app.set('view engine', 'ejs');
+
+// Routes for Lipstick, Mascara, Scrunchie
+const lipstickRoute = require("./routes/lipstick");
+const mascaraRoute = require("./routes/mascara");
+const scrunchieRoute = require("./routes/scrunchie");
+
+app.use('/lipstick', lipstickRoute);
+app.use('/mascara', mascaraRoute);
+app.use('/scrunchie', scrunchieRoute);
+
+// 404 Middleware
 app.use((req, res, next) => {
-    next(error(404, "Resource Not Found"));
-  });
+    next({ status: 404, message: "Resource Not Found" });
+});
 
-//Error Handler Middleware
+// Error Handler Middleware
 app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.json({ error: err.message });
-  });
-
-  // View engine setup
-app.set('view engine', 'ejs');
-
-// With middleware
-app.use('/', function (req, res, next) {
-
-    res.render('User')
-
-   next();
 });
 
-app.get('/', function (req, res) {
-
-    console.log("Render Working")
-
-     res.send();
-})
-
-
-
-  //Listening
+// Listening
 app.listen(port, () => {
-  console.log(`Server listening on port: ${port}.`);
+    console.log(`Server listening on port: ${port}.`);
 });
