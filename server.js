ddcf2ba8534b759f861b9b72926bdd324906917b
Tshiamo Tsoku <ttsoku@icloud.com>
2024-02-27 19:49:37 -0800

    Routes update

--- server.js
+++ server.js
@@ -1,52 +1,59 @@
-
 const express = require("express");
-// const logger = require('./middleware/logger');
-// const errorHandler = require('./middleware/errorHandler');
-
-
-// // Middleware
-// app.use(logger);
-// app.use(express.json()); // Parse JSON bodies
-// app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
-
-// const users = require("./routes/users");
-// const posts = require("./routes/posts");
-
-// const error = require("./utilities/error");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
+// Middleware
+app.use(express.json()); // Parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+ 
+const users = require("./routes/users");
+const posts = require("./routes/posts");
+const comments = require("./routes/comments");
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+// Logging Middlewaare
+app.use((req, res, next) => {
+    const time = new Date();
 
+    console.log(
+        `-----
+    ${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`
+      );
+      if (Object.keys(req.body).length > 0) {
+        console.log("Containing the data:");
+        console.log(`${JSON.stringify(req.body)}`);
+      }
+      next();
+    });
 
-// // Routes
-// app.use('/api', require('./routes/api'));
+// 404 Miidleware
+app.use((req, res, next) => {
+    next(error(404, "Resource Not Found"));
+  });
 
-// // Error handling middleware
-// app.use(errorHandler);
-
-// Start the server
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
+//Error Handler Middleware
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({ error: err.message });
+  });
 
 // // Get all resources
 // router.get('/resources', (req, res) => {
 //     // Logic to retrieve all resources
 //     res.json({ message: 'Get all resources' });
-// });
+// })
 
 // // Create a new resource
 // router.post('/resources', (req, res) => {
 //     // Logic to create a new resource
 //     res.json({ message: 'Create a new resource' });
-// });
+// }):
+
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}.`);
+});
