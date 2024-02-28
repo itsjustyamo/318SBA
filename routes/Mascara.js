f833d24a2fab470d868f909153cdc13dc77bde19
Tshiamo Tsoku <ttsoku@icloud.com>
2024-02-27 20:14:15 -0800

    Final touches

--- routes/Mascara.js
+++ routes/Mascara.js
@@ -2,3 +2,72 @@ const express = require("express");
 const router = express.Router();
 const mascara = require("../data/mascara");
 const error = require("../middleware/error");
+
+router
+  .route("/")
+  .get((req, res) => {
+    const links = [
+      {
+        href: "mascara/:id", // Changed "lipstick" to "mascara"
+        rel: ":id",
+        type: "GET",
+      },
+    ];
+
+    res.json({ mascara, links }); // Changed "lipstick" to "mascara"
+  })
+  .post((req, res, next) => {
+    if (req.body.title && req.body.curlPattern && req.body.lengthOrVolume && req.body.review) {
+      const newMascara = {
+        curlPattern: req.body.curlPattern,
+        lengthOrVolume: req.body.lengthOrVolume,
+        title: req.body.title,
+        review: req.body.review,
+      };
+
+      mascara.push(newMascara); // Changed "lipstick" to "mascara"
+      res.json(newMascara);
+    } else next(error(400, "Insufficient Data"));
+  });
+
+router
+  .route("/:id")
+  .get((req, res, next) => {
+    const foundMascara = mascara.find((m) => m.id == req.params.id); // Changed "lipstick" to "mascara"
+
+    const links = [
+      {
+        href: `/${req.params.id}`,
+        rel: "",
+        type: "PATCH",
+      },
+      {
+        href: `/${req.params.id}`,
+        rel: "",
+        type: "DELETE",
+      },
+    ];
+
+    if (foundMascara) res.json({ mascara: foundMascara, links }); // Changed "lipstick" to "mascara"
+    else next();
+  })
+  .patch((req, res, next) => {
+    const foundMascara = mascara.find((m) => m.id == req.params.id); // Changed "lipstick" to "mascara"
+
+    if (foundMascara) {
+      for (const key in req.body) {
+        foundMascara[key] = req.body[key];
+      }
+      res.json(foundMascara);
+    } else next();
+  })
+  .delete((req, res, next) => {
+    const index = mascara.findIndex((m) => m.id == req.params.id); // Changed "lipstick" to "mascara"
+
+    if (index !== -1) {
+      const deletedMascara = mascara.splice(index, 1);
+      res.json(deletedMascara[0]);
+    } else next();
+  });
+
+module.exports = router;
--- routes/Scrunchie.js
+++ routes/Scrunchie.js
@@ -2,3 +2,70 @@ const express = require("express");
 const router = express.Router();
 const scrunchie = require("../data/srunchie");
 const error = require("../middleware/error");
+
+router
+  .route("/")
+  .get((req, res) => {
+    const links = [
+      {
+        href: "scrunchie/:id", // Changed "lipstick" to "scrunchie"
+        rel: ":id",
+        type: "GET",
+      },
+    ];
+
+    res.json({ scrunchie, links }); // Changed "lipstick" to "scrunchie"
+  })
+  .post((req, res, next) => {
+    if (req.body.material && req.body.size && req.body.quantityInPack) {
+      const newScrunchie = {
+        id: scrunchie.length + 1, // Changed "lipstick" to "scrunchie"
+        material: req.body.material,
+        size: req.body.size,
+        quantityInPack: req.body.quantityInPack,
+      };
+
+      scrunchie.push(newScrunchie); // Changed "lipstick" to "scrunchie"
+      res.json(newScrunchie);
+    } else next(error(400, "Insufficient Data"));
+  });
+
+router
+  .route("/:id")
+  .get((req, res, next) => {
+    const foundScrunchie = scrunchie.find((s) => s.id == req.params.id); // Changed "lipstick" to "scrunchie"
+
+    const links = [
+      {
+        href: `/${req.params.id}`,
+        rel: "",
+        type: "PATCH",
+      },
+      {
+        href: `/${req.params.id}`,
+        rel: "",
+        type: "DELETE",
+      },
+    ];
+
+    if (foundScrunchie) res.json({ scrunchie: foundScrunchie, links }); // Changed "lipstick" to "scrunchie"
+    else next();
+  })
+  .patch((req, res, next) => {
+    const foundScrunchie = scrunchie.find((s) => s.id == req.params.id); // Changed "lipstick" to "scrunchie"
+
+    if (foundScrunchie) {
+      for (const key in req.body) {
+        foundScrunchie[key] = req.body[key];
+      }
+      res.json(foundScrunchie);
+    } else next();
+  })
+  .delete((req, res, next) => {
+    const index = scrunchie.findIndex((s) => s.id == req.params.id); // Changed "lipstick" to "scrunchie"
+
+    if (index !== -1) {
+      const deletedScrunchie = scrunchie.splice(index, 1);
+      res.json(deletedScrunchie[0]);
+    } else next();
+  });
